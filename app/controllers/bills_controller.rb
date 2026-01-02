e422d4a40a4a968cd52a0a57758c6cf275e858ea
Guruprasaath S <gurukprs@gmail.com>
2026-01-02 18:17:09 +0530

    Controller and Routing

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,5 +1,57 @@
 class BillsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:new, :create, :show]
+  before_action :set_bill, only: [:show]
   def new
+    @bill = Bill.new
+    @bill.bill_items.build
+    @products = Product.all
+  end
+
+  def create
+    @bill = Bill.new(bill_params)
+    build_bill_items
+    
+    if @bill.bill_items.empty?
+      flash.now[:alert] = "Please add at least one product"
+      @products = Product.all
+      render :new and return
+    end
+    @bill.calculate_totals
+
+    if @bill.save
+      redirect_to @bill, notice: "Bill generated successfully"
+    else
+      @products = Product.all
+      render :new
+    end
+  end
+
+  def show
+    @bill_items = @bill.bill_items.includes(:product)
+  end
+
+  private
+  def set_bill
+    @bill = Bill.includes(bill_items: :product).find(params[:id])
+  end
+  
+  def bill_params
+    params.require(:bill).permit(
+      :customer_email,
+      :cash_paid,
+      bill_items_attributes: [:product_id, :quantity, :_destroy]
+    )
+  end
+
+  def build_bill_items
+    @bill.bill_items.each do |item|
+      product = Product.find_by(id: item.product_id)
+      next unless product
+
+      item.unit_price = product.price
+      item.total_price = product.price * item.quantity
+      item.tax_amount = (item.total_price * product.tax_percentage) / 100
+    end
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "bills#new" rescue nil
+  resources :bills, only: [:new, :create, :show]
   namespace :admin do
     resources :products
   end
