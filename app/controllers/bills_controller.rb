599e19c346abe86eefa1e0f799bc5a3f3c1e372f
Guruprasaath S <gurukprs@gmail.com>
2026-01-05 15:01:26 +0530

    Validations to page 1

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -13,12 +13,20 @@ class BillsController < ApplicationController
     build_bill_items
     
     @products = Product.all
+    @denominations = [500, 200, 100, 50, 20, 10]
     
     if @bill.bill_items.empty?
       flash.now[:alert] = "Please add at least one product"
       render :new and return
     end
     @bill.calculate_totals
+    
+    validate_denominations
+    validate_cash_paid
+
+    if @bill.errors.any?
+      render :new and return
+    end
 
     if @bill.save
       redirect_to @bill, notice: "Bill generated successfully"
@@ -54,5 +62,42 @@ class BillsController < ApplicationController
       item.tax_amount = (item.total_price * product.tax_percentage) / 100
     end
   end
+
+  def denomination_params
+    params.fetch(:denominations, {}).transform_values(&:to_i)
+  end
+
+  def validate_denominations
+    total_from_denominations = 0
+
+    denomination_params.each do |value, count|
+      if count < 0
+        @bill.errors.add(:base, "Denomination #{value} count cannot be negative")
+      end
+
+      total_from_denominations += value.to_i * count
+    end
+
+    if @bill.cash_paid.present? && total_from_denominations != @bill.cash_paid.to_f
+      @bill.errors.add(
+        :base,
+        "Total of denominations (#{total_from_denominations}) must equal cash given by customer (#{@bill.cash_paid})"
+      )
+    end
+  end
+
+  def validate_cash_paid
+  return if @bill.cash_paid.blank?
+
+  if @bill.cash_paid < @bill.rounded_total
+    @bill.errors.add(
+      :cash_paid,
+      "must be greater than or equal to bill amount (#{@bill.rounded_total})"
+    )
+  end
 end
+
+
+end
+
 
